1aaa88a3dc12845730e23095289db1f7a6c7f359
HeH-Gianni <gianni.tricarico@heh.be>
2024-10-10 13:12:48 +0200

    Ajouter une réponse 'Problem detail' avec le champ supplémentaire errors

--- src/main/java/be/heh/demoba3/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/be/heh/demoba3/exceptionhandler/GlobalExceptionHandler.java
@@ -1,9 +1,14 @@
 package be.heh.demoba3.exceptionhandler;
 
+import be.heh.demoba3.web.ConstraintViolation;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
@@ -14,6 +19,20 @@ public class GlobalExceptionHandler {
     ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
         ProblemDetail validationProblemDetail =
                 ProblemDetail.forStatusAndDetail(BAD_REQUEST, "Validation error");
+
+        List<ConstraintViolation> errors = exception.getFieldErrors()
+                .stream()
+                .map(violation -> ConstraintViolation.builder()
+                        .message(violation.getDefaultMessage())
+                        .fieldName(violation.getField())
+                        .rejectedValue(Objects.isNull(
+                                violation.getRejectedValue()) ?
+                                "null":
+                                violation.getRejectedValue().toString())
+                        .build())
+                .collect(Collectors.toList());
+
+        validationProblemDetail.setProperty("errors", errors);
         return validationProblemDetail;
     }
 }
